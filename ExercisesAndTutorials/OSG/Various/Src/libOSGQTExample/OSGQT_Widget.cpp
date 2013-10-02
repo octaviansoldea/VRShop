30422ca70ea4ce5146808b32c80642770d60538e
Octavian Soldea <octaviansoldea@gmail.com>
2013-10-03 00:12:07 +0300

    minor change 4 debuggin in OSGQT_widget.cpp

--- ExercisesAndTutorials/OSG/Various/Src/libOSGQTExample/OSGQT_Widget.cpp
+++ ExercisesAndTutorials/OSG/Various/Src/libOSGQTExample/OSGQT_Widget.cpp
@@ -22,12 +22,21 @@ OSGQT_Widget::OSGQT_Widget(QWidget * apQWidget, Qt::WindowFlags f) : QWidget(apQ
 	m_viewer.setThreadingModel(osgViewer::Viewer::SingleThreaded);
 	
 	osg::Camera* m_pCamera = m_viewer.getCamera();
+	
 	m_pCamera->setGraphicsContext(m_pGraphicsWindow);
 	m_pCamera->setProjectionResizePolicy(osg::Camera::FIXED); //To preserve the shape of elements
 	m_pCamera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
 	m_pCamera->setViewMatrix(osg::Matrix::identity());
 	m_pCamera->setViewport(new osg::Viewport(0, 0, width(), height()));
 	
+	
+		osg::Vec3 _homeCenter = osg::Vec3(0.275, -6.13, -0.27);
+	osg::Vec3 _homeEye = osg::Vec3(0.275, 0.27, 0.26917);
+	osg::Vec3 _homeUp = osg::Vec3(0, 0, 1);
+	//pManipulator->setHomePosition(-_homeEye, _homeCenter, _homeUp);
+
+	m_pCamera->setViewMatrixAsLookAt(_homeEye, _homeCenter, _homeUp);
+
 	startTimer(10);
 }
 
@@ -53,6 +62,8 @@ void OSGQT_Widget::setSceneData(osg::Node* apNode)	{
 
 void OSGQT_Widget::setCameraManipulator(osgGA::CameraManipulator* manipulator, bool resetPosition)	{
 	m_viewer.setCameraManipulator(manipulator, resetPosition);
+	osgGA::CameraManipulator * pManipulator = manipulator;//m_viewer.getCameraManipulator();
+	
 }
 
 //--------------------------------------------------------------------

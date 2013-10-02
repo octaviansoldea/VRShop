0dcd992f6a5d3af4505c240f9e14e9f08798e337
Octavian Soldea <octaviansoldea@gmail.com>
2013-10-02 23:46:40 +0300

    data added

--- ExercisesAndTutorials/OSG/Various/Src/Test_libCamera/main.cpp
+++ ExercisesAndTutorials/OSG/Various/Src/Test_libCamera/main.cpp
@@ -6,8 +6,8 @@
 using namespace VR;
 
 int main(int argc, char * argv[])	{
-	osg::ref_ptr<osg::Node> model1 = osgDB::readNodeFile("C:/Programi/OSG/OpenSceneGraph-Data-3.0.0/cessna.osg");
-	osg::ref_ptr<osg::Node> model2 = osgDB::readNodeFile("C:/Programi/OSG/OpenSceneGraph-Data-3.0.0/cow.osg");
+	osg::ref_ptr<osg::Node> model1 = osgDB::readNodeFile("../../Resources/cessna.osg");
+	osg::ref_ptr<osg::Node> model2 = osgDB::readNodeFile("../../Resources//cow.osg");
 
 	osg::ref_ptr<osg::Group> root = new osg::Group;
 	root->addChild(model1.get());
